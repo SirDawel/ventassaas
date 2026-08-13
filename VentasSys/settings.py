39ddfbc8cc8834568ded25ba68576d1636ba9927
ApiXtreme Developer <developer@apixtreme.com>
2026-08-13 14:09:13 -0400

    Actualizar configuración en settings.py

--- VentasSys/settings.py
+++ VentasSys/settings.py
@@ -284,7 +284,9 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Use WhiteNoise to serve static files in development/production when configured
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
