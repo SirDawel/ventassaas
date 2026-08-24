bb1d8145df79de0d61cd8579c19c4407cfdfdce6
ApiXtreme Developer <developer@apixtreme.com>
2026-08-24 13:39:17 -0400

    Actualizar cambios actuales

--- ventasweb/models.py
+++ ventasweb/models.py
@@ -52,7 +52,10 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(email, password, **extra_fields)
 
+from django.contrib.auth.models import BaseUserManager
+from django.db import models
 
+from .tenant_models import Client, Domain
 class Client(TenantMixin):
     nombre = models.CharField(max_length=100)
     # schema_name ya lo hereda de TenantMixin, no necesitas redundarlo a menos que agregues validators custom.
